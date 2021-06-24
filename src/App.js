6cb31099115e1a683f289b316702c66bf7953cd2
Arseni Dmitriev <arseni@Arsenis-MacBook-Pro.local>
2021-06-23 20:25:45 -0400

    Added react-router for menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <HomePage></HomePage>
+      <Route exact path='/' component={HomePage}/>
     </div>
   );
 }
