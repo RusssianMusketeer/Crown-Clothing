0b06e5514d41747099511cb5a2b278cc2354fbd3
Arseni Dmitriev <arseni@Arsenis-MacBook-Pro.local>
2021-07-06 14:05:28 -0400

    added checkout component page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,11 +4,21 @@ const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
     [selectCart],
-    (cart) => cart.Items
+    (cart) => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    (cart) => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
-    cartItems => cartItems.reduce((accumulatedQuantity,cartItem) => 
-    accumulatedQuantity + cartItem.quantity, 0)
+    cartItems => cartItems.reduce((accumulatedQuantity,cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector (
+    [selectCartItems],
+    cartItems => cartItems.reduce((accumulatedQuantity,cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
+
+)
